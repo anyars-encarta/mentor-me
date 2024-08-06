64ec8cac22c5e7dad3622d18b702e3ae87e073f4
anyars-encarta <anyarsencarta@gmail.com>
2024-08-06 16:06:41 +0000

    Update Globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import { cn } from '/@lib/utils';
+import { cn } from "@/lib/utils";
 
 const fontSans = Plus_Jakarta_Sans({ 
   subsets: ["latin"],
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>{children}</body>
     </html>
   );
 }
