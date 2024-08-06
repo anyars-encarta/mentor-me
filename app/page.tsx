b94a408e2331e24ea82a5811c52e15e3275c2107
anyars-encarta <anyarsencarta@gmail.com>
2024-08-06 17:57:58 +0000

    Add logo and side image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
+import MentroForm from "@/components/forms/MentroForm";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className='text-3xl underline'>Home</h1>
-      <Button>Click Me</Button>
+    <div className='flex h-screen max-h-screen'>
+      <section className='remove-scrollbar container my-auto'>
+        <div className='sub-container max-w-[496px]'>
+          <Image
+            src='/assets/icons/logo-full.png'
+            width={1000}
+            height={1000}
+            alt='mentor'
+            className='mb-12 h-10 w-fit'
+          />
+
+          <MentroForm />
+
+          <div className='text-14-regular mt-20 flex justify-between'>
+            <p className='justify-end text-dark-600 xl:text-left'>
+              © 2024 MentorMe
+            </p>
+
+            <Link href='/?admin=true' className='text-green-500'>Admin</Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src='/assets/images/onboarding-img-1.png'
+        width={1000}
+        height={1000}
+        alt='mentor'
+        className='side-img max-w-[50%]'
+      />
     </div>
   )
 }
---
+++ components/forms/MentroForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MentroForm = () => {
+  return (
+    <div className=''>MentroForm</div>
+  )
+}
+
+export default MentroForm
