5953c22cd4f18bf0795ee5e72ba7232219e47a8a
anyars-encarta <anyarsencarta@gmail.com>
2024-08-06 16:47:24 +0000

    Correct globals.css, utils.ts and tailwind.config.mjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
     <div>
       <h1 className='text-3xl underline text-white'>Home</h1>
+      <Button className='text-white'>Click Me</Button>
     </div>
   )
 }
