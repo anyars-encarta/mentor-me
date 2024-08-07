2b9a1c13419c636edfd4eaff6a2862fea8a8b6f2
anyars-encarta <anyarsencarta@gmail.com>
2024-08-07 15:32:41 +0000

    Validate form fields with zod

--- components/forms/MentorForm.tsx
+++ components/forms/MentorForm.tsx
@@ -3,9 +3,11 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValivation } from "@/lib/validation"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -17,21 +19,19 @@ export enum FormFieldType {
     SEKELETON = 'skeleton',
 };
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-})
-
 const MentorForm = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const [isLoading, setIsLoading] = useState(false);
+
+    const form = useForm<z.infer<typeof UserFormValivation>>({
+        resolver: zodResolver(UserFormValivation),
         defaultValues: {
-            username: "",
+            name: "",
+            email: '',
+            phone: '',
         },
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    function onSubmit(values: z.infer<typeof UserFormValivation>) {
         console.log(values)
     }
 
@@ -71,7 +71,7 @@ const MentorForm = () => {
                     placeholder='+234 012 345 6789'
                 />
 
-                <Button type="submit">Submit</Button>
+                <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
             </form>
         </Form>
     )
