66aa0127101d4db4e13ad5265af4c4a3ee06a15c
anyars-encarta <anyarsencarta@gmail.com>
2024-08-09 16:36:05 +0000

    Install shadcn alert dialog

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import MentorForm from "@/components/forms/MentorForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-const Home = () => {
+const Home = ({ searchParams}: SearchParamProps) => {
+  const isAdmin = searchParams.admin ==='true';
+
   return (
     <div className='flex h-screen max-h-screen'>
-      {/* TODO: OTP Verification | PassKeyModal*/}
+      {isAdmin && (
+        <PasskeyModal />
+      )}
 
       <section className='remove-scrollbar container my-auto'>
         <div className='sub-container max-w-[496px]'>
