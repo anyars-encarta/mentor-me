3aaf6132d1bf92154a6d4ae3adafd6d805b8d275
anyars-encarta <anyarsencarta@gmail.com>
2024-08-13 11:17:24 +0000

    Add meet camera icon

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -7,6 +7,12 @@ import { Appointment } from "@/types/appwrite.types"
 import { formatDateTime } from "@/lib/utils"
 import AppointmentModal from "../AppointmentModal"
 import Image from "next/image"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 export const columns: ColumnDef<Appointment>[] = [
     {
@@ -65,65 +71,95 @@ export const columns: ColumnDef<Appointment>[] = [
         cell: ({ row: { original: data } }) => {
             return (
                 <div className='flex gap-2'>
-                    <AppointmentModal
-                        type='schedule'
-                        menteeId={data.mentee.$id}
-                        userId={data.userId}
-                        appointment={data}
-                        trigger={
-                            <Image
-                                src='/assets/icons/appointments.svg'
-                                height={16}
-                                width={16}
-                                alt='schedule'
-                                className=' size-6 w-fit cursor-pointer' />
-                        }
-                    />
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <AppointmentModal
+                                    type='schedule'
+                                    menteeId={data.mentee.$id}
+                                    userId={data.userId}
+                                    appointment={data}
+                                    trigger={
+                                        <Image
+                                            src='/assets/icons/appointments.svg'
+                                            height={16}
+                                            width={16}
+                                            alt='schedule'
+                                            className=' size-6 w-fit cursor-pointer' />
+                                    }
+                                />
+                            </TooltipTrigger>
+                            <TooltipContent className='bg-yellow-500'>
+                                <p>Schedule appointment</p>
+                            </TooltipContent>
+                        </Tooltip>
 
-                    <AppointmentModal
-                        type='cancel'
-                        menteeId={data.mentee.$id}
-                        userId={data.userId}
-                        appointment={data}
-                        trigger={
-                            <Image
-                                src='/assets/icons/cancelled.svg'
-                                height={16}
-                                width={16}
-                                alt='cancel'
-                                className=' size-6 w-fit cursor-pointer' />
-                        }
-                    />
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <AppointmentModal
+                                    type='cancel'
+                                    menteeId={data.mentee.$id}
+                                    userId={data.userId}
+                                    appointment={data}
+                                    trigger={
+                                        <Image
+                                            src='/assets/icons/cancelled.svg'
+                                            height={16}
+                                            width={16}
+                                            alt='cancel'
+                                            className=' size-6 w-fit cursor-pointer' />
+                                    }
+                                />
+                            </TooltipTrigger>
+                            <TooltipContent className='bg-red-500'>
+                                <p>Cancel appointment</p>
+                            </TooltipContent>
+                        </Tooltip>
 
-                    <AppointmentModal
-                        type='meet'
-                        menteeId={data.mentee.$id}
-                        userId={data.userId}
-                        appointment={data}
-                        trigger={
-                            <Image
-                                src='/assets/icons/meet.png'
-                                height={16}
-                                width={16}
-                                alt='meet'
-                                className=' size-6 w-fit cursor-pointer' />
-                        }
-                    />
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <AppointmentModal
+                                    type='meet'
+                                    menteeId={data.mentee.$id}
+                                    userId={data.userId}
+                                    appointment={data}
+                                    trigger={
+                                        <Image
+                                            src='/assets/icons/meet-cam.png'
+                                            height={16}
+                                            width={16}
+                                            alt='meet'
+                                            className=' size-6 w-fit cursor-pointer rounded-full' />
+                                    }
+                                />
+                            </TooltipTrigger>
+                            <TooltipContent className='bg-blue-500'>
+                                <p>Meet appointment</p>
+                            </TooltipContent>
+                        </Tooltip>
 
-                    <AppointmentModal
-                        type='complete'
-                        menteeId={data.mentee.$id}
-                        userId={data.userId}
-                        appointment={data}
-                        trigger={
-                            <Image
-                                src='/assets/icons/check.svg'
-                                height={16}
-                                width={16}
-                                alt='cancel'
-                                className=' size-6 w-fit cursor-pointer' />
-                        }
-                    />
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <AppointmentModal
+                                    type='complete'
+                                    menteeId={data.mentee.$id}
+                                    userId={data.userId}
+                                    appointment={data}
+                                    trigger={
+                                        <Image
+                                            src='/assets/icons/check.svg'
+                                            height={16}
+                                            width={16}
+                                            alt='cancel'
+                                            className=' size-6 w-fit cursor-pointer rounded-full' />
+                                    }
+                                />
+                            </TooltipTrigger>
+                            <TooltipContent className='bg-green-500'>
+                                <p>Complete appointment</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
                 </div>
             )
         },
