f425ac884345e854e6ad4c76a702790860288eb8
anyars-encarta <anyarsencarta@gmail.com>
2024-08-14 16:35:24 +0000

    Add send SMS notification

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,8 @@
 'use server'
 
 import { ID, Query } from "node-appwrite";
-import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases, messaging } from "../appwrite.config";
+import { formatDateTime, parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
 import { revalidatePath } from "next/cache";
 
@@ -89,10 +89,33 @@ export const updateAppointment = async ({ appointmentId, userId, appointment, ty
             throw new Error('Appointment not found')
         }
 
-        // TODO: SMS Notification
+        // Send SMS Notification
+        const smsMessage = `
+        Hi, its MentorMe. 
+        ${type === 'schedule' 
+            ? `Your appointment has been scheduled for ${formatDateTime(appointment.schedule!).dateTime}`
+        : type === 'cancel' && `We regret to inform you that your appointment has been cancelled for the following reason: ${appointment.cancellationReason}`}
+        `
+
+        await sendSMSNotification(userId, smsMessage);
 
         revalidatePath('/admin');
         return parseStringify(updatedAppointment);
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+    try {
+        const message = await messaging.createSms(
+            ID.unique(),
+            content,
+            [],
+            [userId],
+        )
+
+        return parseStringify(message);
     } catch (e) {
         console.log(e)
     }
