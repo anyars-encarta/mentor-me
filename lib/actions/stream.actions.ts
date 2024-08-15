78c4996cc55138f7cb5fbd9f86cc34e103aeebe8
anyars-encarta <anyarsencarta@gmail.com>
2024-08-15 11:06:41 +0000

    Install clerk

--- lib/actions/stream.actions.ts
+++ lib/actions/stream.actions.ts
@@ -1,16 +1,20 @@
 'use server';
 
-import { getUser } from "./mentee.actions";
+import { StreamClient } from '@stream-io/node-sdk';
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 const apiSecret = process.env.STREAM_SECRET_KEY;
 
-export const tokenProvider = async (userId: string) => {
-    const user = await getUser(userId);
-
-    if (!user) throw new Error('User is not logged in');
+export const tokenProvider = (userId: string) => {
+    if (!userId) throw new Error('User is not logged in');
     if (!apiKey) throw new Error('No API key');
     if (!apiSecret) throw new Error('No API Secret');
 
-    const streamClient = new streamClient
+    const client = new StreamClient(apiKey, apiSecret);
+    const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
+    const issued = Math.floor(Date.now() / 1000) - 60;
+
+    const token = client.createToken(userId, exp, issued);
+
+    return token;
 }
