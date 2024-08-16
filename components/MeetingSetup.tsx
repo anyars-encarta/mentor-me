3eddd796de8b99b8652d1d892716b0e79c89c829
anyars-encarta <anyarsencarta@gmail.com>
2024-08-16 10:21:02 +0000

    Add camera and microphone toggle on and off

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -11,7 +11,7 @@ const MeetingSetup = () => {
   if (!call) {
     throw new Error('useCall must be used within StreamCall component')
   };
-  
+
   useEffect(() => {
     if (isMicCamToggledOn) {
       call?.camera.disable();
@@ -26,6 +26,17 @@ const MeetingSetup = () => {
     <div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
         <h1 className='text-2xl font-bold'>Setup</h1>
         <VideoPreview />
+
+        <div className='flex h-16 items-center justify-center gap-3'>
+          <label className='flex items-center justify-center gap-2 font-medium'>
+            <input 
+            type='checkbox'
+            checked={isMicCamToggledOn}
+            onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
+            />
+            Join with mic and camera off
+          </label>
+        </div>
     </div>
   )
 }
